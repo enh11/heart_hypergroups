8c50f00a0eb54085d32e447f26cd0547b36c8286
Enh <enriconh@gmail.com>
2025-08-12 09:12:50 +0200

    weak associativity
    Added is_weak_associativity function

--- is_weak_associative.m
+++ is_weak_associative.m
@@ -1,9 +1,14 @@
 function result = is_weak_associative(H, M)
+result = true;
     for a  = H
         for b  = H 
             for c  = H 
-                for d  = H 
-
+                ab_c = set_operation(H,M,
+                    hyperoperation(H,M,a,b),c);
+                a_bc = set_operation(H,M,a,hyperoperation(H,M,b,c));
+                if isempty(intersect(a_bc,ab_c))
+                    result = false;
+                    return;
                 end
             end
         end
