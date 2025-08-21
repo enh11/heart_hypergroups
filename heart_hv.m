63b47fe08f227a783438e873530995b1b1745875
Enh <enriconh@gmail.com>
2025-08-21 10:33:12 +0200

    Fixed heart_hv

--- heart_hv.m
+++ heart_hv.m
@@ -1,6 +1,9 @@
 function result = heart_hv(H, M)
+beta  = beta_relation(H,M);
+beta_star = transitive_closure(H,beta);
+    quotient_set = quotient_set(H,M,beta_star)
     fg = fundamental_group(H,M);
-    identities =collect_identities(H,M);
+    identities =collect_identities(quotient_set,fg);
 
     for k=1:length(identities)
         if identities{k}
